765094771b5e3e005ceab78dcf653e3f73a8fab8
MarcioCostaRJr <marcio_crj@hotmail.com>
2019-12-18 18:19:44 -0300

    parando no ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 import { AlertaService } from './alerta.service';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListaPessoaComponent ],
+  declarations: [ AppComponent, HelloComponent, ListaPessoaComponent, InterpolationBindingComponent, PropertyBindingComponent, TwoWayBindingComponent, NgIfComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AlertaService]
 })
