1ff7faaf853a6b7611e799d77d8d9ba274c333e1
MarcioCostaRJr <marcio_crj@hotmail.com>
2019-12-29 23:31:57 -0300

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { InterpolationBindingComponent } from './interpolation-binding/interpola
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ListaPessoaComponent, InterpolationBindingComponent, PropertyBindingComponent, TwoWayBindingComponent, NgIfComponent ],
+  declarations: [ AppComponent, HelloComponent, ListaPessoaComponent, InterpolationBindingComponent, PropertyBindingComponent, TwoWayBindingComponent, NgIfComponent, NgSwitchCaseComponent, NgForComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AlertaService]
 })
